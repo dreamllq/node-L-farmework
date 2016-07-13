4c08c20eb244a41592b4f69ce26eaff74559212c
lvliqi <lvliqi@360.com>
2016-07-13 18:12:37 +0800

    1

--- Farmework/Util/R/RedisCache.js
+++ Farmework/Util/R/RedisCache.js
@@ -9,12 +9,50 @@ bluebird.promisifyAll(redis.Multi.prototype);
 var config = Config();
 
 var Redis = function () {
-    var client = redis.createClient(config.memory_port, config.memory_host, {
-        no_ready_check: true
+    var client = redis.createClient({
+        host: config.memory_host,
+        port: config.memory_port,
+        no_ready_check: true,
+        retry_strategy: function (options) {
+            if (options.error.code === 'ECONNREFUSED') {
+                // End reconnecting on a specific error and flush all commands with a individual error
+                return new Error('The server refused the connection');
+            }
+            if (options.total_retry_time > 1000 * 60 * 60) {
+                // End reconnecting after a specific timeout and flush all commands with a individual error
+                return new Error('Retry time exhausted');
+            }
+            if (options.times_connected > 10) {
+                // End reconnecting with built in error
+                return undefined;
+            }
+            // reconnect after
+            return Math.max(options.attempt * 100, 3000);
+        }
+    });
+
+    client.on("ready", function () {
+        console.log("redis client ready");
+    });
+
+    client.on("connect", function () {
+        console.log("redis client connect");
+    });
+
+    client.on("reconnecting", function () {
+        console.log("redis client reconnecting");
     });
 
     client.on("error", function (err) {
         console.error(err);
+    });
+
+    client.on("end", function () {
+        console.log("redis client close");
+    });
+
+    client.on("warning", function () {
+        console.log("redis client warning");
     });
 
     return client;
