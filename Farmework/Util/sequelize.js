d82764a0cd71fa30ab2f50f413ef010545392117
lvliqi <lvliqi@360.com>
2016-07-14 17:33:15 +0800

    fix bug

--- Farmework/Util/sequelize.js
+++ Farmework/Util/sequelize.js
@@ -6,6 +6,7 @@ var config = Config();
 
 var sequelize = new Sequelize(config.db_database, config.db_username, config.db_password, {
     host: config.db_host,
+    port: config.db_port,
     dialect: config.db_type,
     query: {
         raw: true
